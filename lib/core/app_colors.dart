1abd8f5309b519ab55df6f9fc3379701db04c287
Renzo Gabriel Fajardo <44675344+renzo611@users.noreply.github.com>
2025-08-10 12:05:32 -0300

    feat: add app colors and env tempalte

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class AppColors {
+  // 🌞 Light Theme
+  static const Color primaryLight = Color(0xFF1E88E5); // Dodger Blue
+  static const Color primaryDarkLight = Color(0xFF1565C0); // Cobalt Blue
+  static const Color secondaryLight = Color(0xFFFFC107); // Amber
+  static const Color backgroundLight = Color(0xFFFFFFFF); // White
+  static const Color surfaceLight = Color(0xFFF5F5F5); // Cultured
+  static const Color errorLight = Color(0xFFD32F2F); // Firebrick
+
+  static const Color onPrimaryLight = Color(0xFFFFFFFF); // White
+  static const Color onSecondaryLight = Color(0xFF000000); // Black
+  static const Color onBackgroundLight = Color(0xFF000000); // Black
+  static const Color onSurfaceLight = Color(0xFF000000); // Black
+
+  // 🌙 Dark Theme
+  static const Color primaryDark = Color(0xFF90CAF9); // Baby Blue
+  static const Color primaryDarkDark = Color(0xFF42A5F5); // Sky Blue
+  static const Color secondaryDark = Color(0xFFFFD54F); // Saffron
+  static const Color backgroundDark = Color(0xFF121212); // Rich Black
+  static const Color surfaceDark = Color(0xFF1E1E1E); // Eerie Black
+  static const Color errorDark = Color(0xFFEF5350); // Light Coral Red
+
+  static const Color onPrimaryDark = Color(0xFF000000); // Black
+  static const Color onSecondaryDark = Color(0xFF000000); // Black
+  static const Color onBackgroundDark = Color(0xFFFFFFFF); // White
+  static const Color onSurfaceDark = Color(0xFFFFFFFF); // White
+}
